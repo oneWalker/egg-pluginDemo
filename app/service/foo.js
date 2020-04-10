12e6e9a2dc7d40e01b9e3f848999b8f06bc2dc9d
xusy <xusy@knownsec.com>
2020-04-10 15:05:57 +0800

    fix:i

--- app/service/foo.js
+++ app/service/foo.js
@@ -3,7 +3,7 @@
 // app/service/user.js
 const Service = require('egg').Service;
 
-class FooService extends Service {i
+class FooService extends Service {
     async incrAndReturn(id) {
         await this.ctx.model.Foo.update({ id }, { $inc: { count: 1 } }, { upsert: true });
         return (await this.ctx.model.Foo.findOne({ id })).count;
