e8aecf8bda696e445441b3f1472d2bc0180d1c87
xusy <xusy@knownsec.com>
2020-04-10 14:58:17 +0800

    按命名规范处理方法名

--- app/service/foo.js
+++ app/service/foo.js
@@ -3,8 +3,8 @@
 // app/service/user.js
 const Service = require('egg').Service;
 
-class FooService extends Service {
-    async IncrAndReturn(id) {
+class FooService extends Service {i
+    async incrAndReturn(id) {
         await this.ctx.model.Foo.update({ id }, { $inc: { count: 1 } }, { upsert: true });
         return (await this.ctx.model.Foo.findOne({ id })).count;
     }
