11339e5495bf93fbb25cfd3a22be0ae247c6b218
liukun <liukun@mobimedical.com>
2020-04-14 09:50:14 +0800

    fix the bugs about router-plus using

--- app/router/foo.js
+++ app/router/foo.js
@@ -5,8 +5,8 @@ module.exports = app => {
 
     // version
     //使用egg-router-plus优化后的写法
-    const apiRouter = router.namespace('/v1/public/');
-    apiRouter.get('/foo/access',controller.foo.access);
+    const apiRouter = router.namespace('/v1/public/foo/');
+    apiRouter.get('access',controller.foo.access);
 
     //原始写法仍然保留
     //router.get('/public/foo/plugin', controller.foo.pluginDemo);
